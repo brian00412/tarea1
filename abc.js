f2f7fc6440065d41620c8c299dbc604abfce38e8
ramox-mq <daniel.rsi6oa@gmail.com>
2023-06-27 20:42:47 -0300

    agregue un archivo

---
+++ abc.js
@@ -0,0 +1,10 @@
+let nacimientoPedro = 2002;
+let anoActual = 2020;
+let edadCarnetConducir = 18;
+let edadPedro = anoActual - nacimientoPedro;
+
+if (edadPedro >= edadCarnetConducir) {
+    console.log("Pedro si se pude sacar el carnet de conducir")
+} else {
+    console.log("Pedro no se puede sacar el carnet de conducir")
+}
